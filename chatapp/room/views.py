116ff58e9d81540e096ce404ea6c0ad6091d9c5b
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-04-28 16:01:56 +0600

    Chatting App

--- chatapp/room/views.py
+++ chatapp/room/views.py
@@ -12,7 +12,8 @@ def rooms(request):
 
 @login_required
 def room(request, slug):
-    return render(request, 'room/room.html', {
-        'room': Room.objects.get(slug=slug),
-        'messages': Message.objects.filter(room=room)[0:25]
-    })
+
+    room =  Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
+
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
