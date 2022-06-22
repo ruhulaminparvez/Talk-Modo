36391f8b2059de3589bbae7f3e76e4d5f2720179
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-06-22 15:45:58 +0600

    Add Site Icon and Home Image :octocat:

--- chatapp/room/admin.py
+++ chatapp/room/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 from . models import Room, Message
 
 # Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('room', 'user', 'content', 'date_added')
+    list_filter = ('room', 'user')
+    search_fields = ('content','room')
+
 admin.site.register(Room)
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
